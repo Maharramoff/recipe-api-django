e3f5cb7120a21a096ac0a0f0464b77d1d1d99eb0
Maharramoff <sh.maharramov@gmail.com>
2020-08-10 22:22:52 +0400

    add: test if user exists

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -28,3 +28,12 @@ class PublicUserApiTests(TestCase):
         user = get_user_model().objects.get(**res.data)
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        payload = {
+            'email': 'test@demo.az',
+            'password': 'testpass',
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
