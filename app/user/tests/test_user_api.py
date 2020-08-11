e33dd9bd36f638f75dd4b19b6bacead62d34c63d
Maharramoff <sh.maharramov@gmail.com>
2020-08-11 20:14:29 +0400

    add: test if password length valid

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -37,3 +37,15 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        payload = {
+            'email': 'test@demo.az',
+            'password': 'testpass',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
