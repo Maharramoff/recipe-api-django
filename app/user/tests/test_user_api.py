500705be82b8e60285bd3303384b9832ebda64e4
Maharramoff <sh.maharramov@gmail.com>
2020-08-11 20:59:20 +0400

    fix: password length test typo

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -41,7 +41,7 @@ class PublicUserApiTests(TestCase):
     def test_password_too_short(self):
         payload = {
             'email': 'test@demo.az',
-            'password': 'testpass',
+            'password': 'ss',
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
